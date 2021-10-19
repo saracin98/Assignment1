e3b41a70ff4b2341d58422c8c60c268ff9f609fd
Robert Saracin <robert.saracin@smarovial.com>
2021-10-19 13:21:38 +0300

    feature1 modified

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("FirstUser");
-            Console.WriteLine("New line from SecondUser");
+            Console.WriteLine("FirstUser modified line");
         }
     }
 }
